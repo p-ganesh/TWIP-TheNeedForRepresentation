fad54dd855fd72c214aeb997622a12c4f7e76323
p-ganesh <pg304@pvsd.org>
2018-09-24 10:48:52 -0400

    bennito martinkolini

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,20 @@
 package com.company;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner kbreader = new Scanner(System.in);
+
+        System.out.println("Enter the word you would like encrypted.");
+        String word = kbreader.next();
+
+        int length = word.length();
+        int k = 0;
+        int number = Math.random()*5;
+
+        for (k <= length) {
+
+        }
     }
 }
