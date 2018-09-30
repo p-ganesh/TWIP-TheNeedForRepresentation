480118302a5f0a7790c527ec8d27857addaeb0c6
p-ganesh <41019754+p-ganesh@users.noreply.github.com>
2018-09-30 15:37:51 -0400

    if you win you can't lose

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,20 +1,46 @@
 package com.company;
-import java.util.Scanner;
+import java.util.*;
 
-public class Main {
+public class Main
+{
 
-    public static void main(String[] args) {
-        Scanner kbreader = new Scanner(System.in);
-
+    public static void main(String[] args)
+    {
+        Scanner kb = new Scanner(System.in);
         System.out.println("Enter the word you would like encrypted.");
-        String word = kbreader.next();
+        String word = kb.next();
 
-        int length = word.length();
-        int k = 0;
-        int number = Math.random()*5;
+        int percent = (int)(Math.random()*word.length());
+        String word2= word.substring(percent)+word.substring(0,percent);
 
-        for (k <= length) {
+        int start = (int)(Math.random()*5);
 
+        for(int j=1;j<=word.length();j++)
+        {
+            String output;
+            char out= word2.charAt(j-1);
+            if(start%5==0)
+            {
+                output=Integer.toBinaryString(out);
+            }
+            else if(start%5==1)
+            {
+                output=Integer.toOctalString(out);
+            }
+            else if(start%5==2)
+            {
+                output=Integer.toString(out);
+            }
+            else if(start%5==3)
+            {
+                output= Integer.toHexString(out);
+            }
+            else
+            {
+                output = Character.toString(out);
+            }
+            start++;
+            System.out.print(output);
         }
     }
 }
